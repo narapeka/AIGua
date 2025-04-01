31e936d3e7c1ff0a98a0399705e668dd6470a616
narapeka <narapeka@hotmail.com>
2025-04-01 18:06:45 +0800

    test refactor of movie

--- backend/app/routers/files.py
+++ backend/app/routers/files.py
@@ -5,6 +5,7 @@ from .file_identify import router as identify_router
 from .file_renamer import router as renamer_router
 from .tmdb_service import router as tmdb_router
 from .tv_shows import router as tv_router
+from .movies import router as movie_router
 
 # Create a combined router
 router = APIRouter()
@@ -14,4 +15,5 @@ router.include_router(scanner_router)
 router.include_router(identify_router)
 router.include_router(renamer_router)
 router.include_router(tmdb_router)
-router.include_router(tv_router) 
+router.include_router(tv_router)
+router.include_router(movie_router) 
